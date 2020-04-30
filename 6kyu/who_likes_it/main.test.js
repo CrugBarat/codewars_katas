0c856c920bcd4a1b9b1b8a74cd1601ef7a4b8ecb
Craig Barratt <craigtbarratt@gmail.com>
2020-05-01 00:01:32 +0100

    PASS but needs refactored

---
+++ 6kyu/who_likes_it/main.test.js
@@ -0,0 +1,18 @@
+const likes = require('./main.js')
+
+describe("Who Likes It?", () => {
+
+  test('T1', () => {
+    expect(likes([])).toBe("no one likes this")
+  })
+
+  test('T2', () => {
+    expect(likes(["Max", "John", "Mark"])).toBe("Max, John and Mark like this")
+  })
+
+  test('T3', () => {
+    expect(likes(["Alex", "Jacob", "Mark", "Max"])).toBe("Alex, Jacob and 2 others like this")
+  })
+
+
+})
