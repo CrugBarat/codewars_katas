3b844a31c73327a0b68c31836868dd77725d7b42
Craig Barratt <craigtbarratt@gmail.com>
2020-04-30 20:39:08 +0100

    PASS

---
+++ 5kyu/the_hashtag_gen/main.test.js
@@ -0,0 +1,21 @@
+const generateHashtag = require('./main.js')
+
+describe("The Hashtag Generator", () => {
+
+  test('T1', () => {
+    expect(generateHashtag(" Hello there thanks for trying my Kata" )).toBe("#HelloThereThanksForTryingMyKata")
+  })
+
+  test('T2', () => {
+    expect(generateHashtag("    Hello     World   " )).toBe("#HelloWorld")
+  })
+
+  test('T3', () => {
+    expect(generateHashtag("")).toBe(false)
+  })
+
+  test('T4', () => {
+    expect(generateHashtag("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false)
+  })
+
+})
