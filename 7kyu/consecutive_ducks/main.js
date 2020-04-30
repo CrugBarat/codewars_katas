57f484292c9d89c99856b0588bae8d62bec6b629
Craig Barratt <craigtbarratt@gmail.com>
2020-04-30 10:23:56 +0100

    PASS

---
+++ 7kyu/consecutive_ducks/main.js
@@ -0,0 +1,8 @@
+// Given Positive integer, N , Return true if it could be expressed as a sum of two or more consecutive positive numbers , OtherWise return false .
+
+// Notes
+// Guaranteed constraint : 2 ≤ N ≤ (2^32) -1
+
+let consecutiveDucks = num => !Number.isInteger( Math.log2(num));
+
+module.exports = consecutiveDucks
---
+++ 7kyu/consecutive_ducks/main.test.js
@@ -0,0 +1,18 @@
+const consecutiveDucks = require('./main.js')
+
+describe("IQ Tests", () => {
+
+  test('T1', () => {
+    expect(consecutiveDucks(9)).toBe(true)
+  })
+
+  test('T2', () => {
+    expect(consecutiveDucks(64)).toBe(false)
+  })
+
+  test('T3', () => {
+    expect(consecutiveDucks(42)).toBe(true)
+  })
+
+
+})
