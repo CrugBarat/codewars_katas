02a114b4237490e5a71144d1acc4e6d5233b4853
Craig Barratt <craigtbarratt@gmail.com>
2020-04-30 04:59:16 +0100

    failing odd nums

---
+++ 6kyu/iq_test/main.test.js
@@ -0,0 +1,18 @@
+const iqTest = require('./main.js')
+
+describe("IQ Tests", () => {
+
+  test('T1', () => {
+    expect(iqTest('2 4 7 8 10')).toBe(3)
+  })
+
+  test('T2', () => {
+    expect(iqTest('1 2 2')).toBe(1)
+  })
+
+  test('T3', () => {
+    expect(iqTest('1 2 1 1')).toBe(2)
+  })
+
+
+})
