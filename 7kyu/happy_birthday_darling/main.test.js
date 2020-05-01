61c61058a28fd2aec265c5e0caf530aad81fdab2
Craig Barratt <craigtbarratt@gmail.com>
2020-05-01 11:24:02 +0100

    PASS

---
+++ 7kyu/happy_birthday_darling/main.test.js
@@ -0,0 +1,25 @@
+const womensAge = require('./main.js')
+
+describe("Happy Birthday Darling", () => {
+
+  test('T1', () => {
+    expect(womensAge(32)).toBe( "32? That's just 20, in base 16!")
+  })
+
+  test('T2', () => {
+    expect(womensAge(39)).toBe("39? That's just 21, in base 19!")
+  })
+
+  test('T3', () => {
+    expect(womensAge(22)).toBe("22? That's just 20, in base 11!")
+  })
+
+  test('T4', () => {
+    expect(womensAge(65)).toBe("65? That's just 21, in base 32!")
+  })
+
+  test('T5', () => {
+    expect(womensAge(83)).toBe("83? That's just 21, in base 41!")
+  })
+
+})
