6ede21c2dafb324115c42221835beaad0f7a102d
Craig Barratt <craigtbarratt@gmail.com>
2020-05-01 10:55:01 +0100

    PASS all personal tests. CW doesn't recognise BigInt - BOO

---
+++ 4kyu/sum_strings/main.js
@@ -0,0 +1,14 @@
+// Given the string representations of two integers, return the string representation of the sum of those integers.
+//
+// For example:
+//
+// sumStrings('1','2') // => '3'
+// A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+let sumStrings = (a, b) => {
+  return BigInt((BigInt(b) + BigInt(a))).toString();
+}
+
+sumStrings('', '5')
+
+module.exports = sumStrings
